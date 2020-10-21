9a485275da7751ac78e859022af5dbb192834911
Cika <cika.lolita@hotmail.com>
2020-10-21 12:37:17 +0200

    removed unnecesarry code

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,31 +2,6 @@ import React from "react";
 import axios from "axios";
 import CharacterCard from "./CharacterCard";
 
-// export default function Characterlist() {
-//     const [CharacterList, GetCharacterList] = useState([]);
-
-//     useEffect(() => {
-//         axios
-//             .get('https://rickandmortyapi.com/api/character/')
-//             .then(response => {
-//                 console.log("RESPONSE HERE", response.data.results);
-//                 GetCharacterList(response.data.results)
-//             })
-//             .catch(err => {
-//                 console.log(err)
-//              });
-//         }, []);
-
-//     return (
-//         <section className="character-list">
-//             <h2>Welcome to Rick and Morty Land</h2>
-//             {CharacterList.map((chars, id) => {
-//                 return <CharacterCard key={id} chars={chars} />;
-//             })}
-//         </section>
-//     );
-// }
-
 export default class CharacterList extends React.Component {
   state = {
     characters: [],
