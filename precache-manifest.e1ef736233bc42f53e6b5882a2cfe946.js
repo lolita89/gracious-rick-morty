269a1d1ea3dca21a05d703e0112a54a9366af550
Cika <cika.lolita@hotmail.com>
2020-10-21 09:53:05 +0200

    Updates

---
+++ precache-manifest.e1ef736233bc42f53e6b5882a2cfe946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d86554aa836183fed5f84e6d363605f",
+    "url": "/gracious-rick-morty/index.html"
+  },
+  {
+    "revision": "c677ad336cc30824a316",
+    "url": "/gracious-rick-morty/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9d948109f5340fdbc4bc",
+    "url": "/gracious-rick-morty/static/js/2.693b568a.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/gracious-rick-morty/static/js/2.693b568a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c677ad336cc30824a316",
+    "url": "/gracious-rick-morty/static/js/main.b365f701.chunk.js"
+  },
+  {
+    "revision": "4b104ab9266ef46d8ee0",
+    "url": "/gracious-rick-morty/static/js/runtime-main.6d504834.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gracious-rick-morty/static/media/logo.5d5d9eef.svg"
+  }
+]);
