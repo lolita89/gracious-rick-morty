4937ba246691031b22d8ca8facd7c1cbf5c21a05
Cika <cika.lolita@hotmail.com>
2020-10-22 09:06:59 +0200

    clean

--- src/components/CharacterList/CharacterCard/NameCard/NameCard.js
+++ src/components/CharacterList/CharacterCard/NameCard/NameCard.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import "../../../../styles/character-list.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
 
 export default class NameCard extends React.Component {
   render() {
@@ -12,6 +15,7 @@ export default class NameCard extends React.Component {
     return (
       <Card.Body>
         <Card.Title href="#" className="title-name" onClick={handleClick}>{this.props.name}</Card.Title>
+        <FontAwesomeIcon icon="check-square"/>
       </Card.Body>
     );
   }
