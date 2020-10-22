04686b4410e2f1a81e90b227ae8d3a2f93bab3d5
Cika <cika.lolita@hotmail.com>
2020-10-22 15:32:28 +0200

    search box styling

--- src/components/CharacterList/CharacterSearchBox/CharacterSearchBox.js
+++ src/components/CharacterList/CharacterSearchBox/CharacterSearchBox.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "../../../styles/character-list.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default class CharacterSearchBox extends React.Component {
   render() {
     return (
-      <input placeholder="search a character name" onChange={this.props.handleInput} type="text"/>
+      <div className="wrapper">
+        <FontAwesomeIcon icon={faSearch} className="search-icon" />
+        <input
+          className="input-text"
+          placeholder="search a character name"
+          onChange={this.props.handleInput}
+          type="text"
+        />
+      </div>
     );
   }
 }
